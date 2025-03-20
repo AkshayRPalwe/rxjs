6bc7da22fa7977ec9078a1d182e32d1b805ea53b
Akshay Palwe <palweakshay20@gmail.com>
2025-03-20 12:16:24 +0530

    6. What is RxJs? What Problem Does it Solve?

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,21 +10,29 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // stream 1
+    // -------------------------------------------------------------------------------------
+    //    Why use RxJS?
+    // -------------------------------------------------------------------------------------
+    // JavaScript applications often deal with multiple asynchronous streams 
+    // (e.g., user interactions, backend requests, timers).
+    // Combining these streams using native JavaScript callbacks leads to callback hell,
+    // making the code difficult to read and maintain.
+    // -------------------------------------------------------------------------------------
+    //    How does RxJS help?
+    // ----------------------------------------------------------------------------------    
+    // RxJS (Reactive Extensions for JavaScript) provides a structured way to combine and manage streams of values.
+    // It replaces deeply nested callbacks with a more maintainable approach using Observables.
+
     document.addEventListener('click', (event) => {
       console.log(event);
+      setTimeout(() => {
+        console.log('finished...');
+        let count = 0;
+        setInterval(() => {
+          console.log(count);
+          count++;
+        }, 1000);
+      }, 3000);
     });
-
-    //stream 2
-    let count = 0;
-    setInterval(() => {
-      console.log(count);
-      count++;
-    }, 1000);
-
-    // stream 3
-    setTimeout(() => {
-      console.log('finished...');
-    }, 3000);
   }
 }
