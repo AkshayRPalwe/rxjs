3da4ee3a581b52a5a7e9f1f399060aa80595560a
Akshay Palwe <palweakshay20@gmail.com>
2025-03-20 12:04:38 +0530

    starter code

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
+  standalone: false,
+})
+export class AboutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
